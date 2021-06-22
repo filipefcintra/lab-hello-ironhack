276a69a9f8f78b0a6946e653d9ab434422071601
filipefcintra <filipefcintra@gmail.com>
2021-06-22 02:09:11 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ironhack from './assets/images/ironhack-logo.svg';
+import menu from './assets/images/menu-top.svg';
+import icon1 from './assets/images/icon1.png';
+import icon2 from './assets/images/icon2.png';
+import icon3 from './assets/images/icon3.png';
+import icon4 from './assets/images/icon4.png';
+import reactlogo from './assets/images/react-logo.svg';
+
 
 function App() {
   return (
     <div className="App">
+        <div className="nav">
+          <img src={ironhack}/>
+          <img src={menu}/>
+        </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div className="header">
+          <h1>
+            Say hello to <br /> ReactJS
+          </h1>
+          </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="subheader">
+          <p>
+            You will learn hot to use
+            <br />
+            the most popular frontend library
+            <br />
+            and become a super Ninja developer.
+          </p>
+        </div>
       </header>
+      <footer className="foot">
+        
+          <img src={icon1}/>
+          <img src={icon2}/>
+          <img src={icon3}/>
+          <img src={icon4}/>
+        
+      </footer>
     </div>
-  );
-}
+    )
+};
 
 export default App;
